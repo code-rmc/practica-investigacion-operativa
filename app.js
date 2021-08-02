32a0428a4529b118be605f832ec9376e2cde151a
code-rmc <75862846+code-rmc@users.noreply.github.com>
2021-08-02 16:14:29 -0300

    correccion
    Bug de notificación, ahora muestra las verdaderas

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 const preguntas = [1, 3, 3, 3, 3, 1, 3, 2, 3, 2, 3, 3, 4, 5, 2, 4, 2, 5, 1, 4, 4, 3];
-let respuestas = [];
-
 
 const formulario = document.getElementById("formulario");
 const sele = document.getElementsByName("pregunta2");
@@ -8,20 +6,22 @@ const btnEnviar = document.getElementById("enviar");
 const toastBody = document.querySelector(".toast-body");
 
 
+function correccion(res) {
+    return res.map((valor, i) => Number(valor) === preguntas[i] ? true : false);
+}
+
 formulario.addEventListener("submit", (e) => {
     e.preventDefault();
     //btnEnviar.disabled = true;
 
     const dataForm = new FormData(formulario);
 
-    respuestas = [];
-
-    for (let i = 1; i <= preguntas.length; i++) {
-        respuestas = [...respuestas, dataForm.get(`pregunta${i}`)];
-    }
+    const respuestas = preguntas.map((value, i) => dataForm.get(`pregunta${i + 1}`));
 
     const contador = { v: 0, f: 0 };
-    const correctas = correccion();
+
+    const correctas = correccion(respuestas);
+
 
     for (let i = 1; i <= preguntas.length; i++) {
 
@@ -29,6 +29,7 @@ formulario.addEventListener("submit", (e) => {
 
         document.getElementsByName(`pregunta${i}`).forEach((value) => {
             let res = document.createElement("span");
+            let advert = document.createElement("span");
 
             if (correctas[i - 1]) {
                 res.classList.add("badge", "bg-success");
@@ -36,19 +37,19 @@ formulario.addEventListener("submit", (e) => {
             } else {
                 res.classList.add("badge", "bg-danger");
                 res.innerHTML = '<i class="fas fa-times"></i>';
+
+                advert.classList.add("badge", "bg-primary");
+                advert.innerHTML = '<i class="fas fa-exclamation-circle"></i>';
+                Number(value.value) === preguntas[i - 1] ? value.appendChild(advert) : null;
             }
             value.checked ? value.appendChild(res) : null;
         });
     }
 
+    toastBody.innerHTML = "";
     let totalRespuesta = document.createElement("p");
+
     totalRespuesta.innerHTML = `<span class="badge bg-success"><i class="fas fa-check"></i></span> Verdaderas: ${contador.v} </br> <span class="badge bg-danger"><i class="fas fa-times"></i></span> Flasos: ${contador.f}`;
+
     toastBody.appendChild(totalRespuesta);
-
 });
-
-function correccion() {
-    return respuestas.map((valor, i) => {
-        return valor == preguntas[i] ? true : false;
-    });
-}
