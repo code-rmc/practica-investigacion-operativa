be47c016941f84ee6d42cdbc51f9b0b4c24cb978
code-rmc <75862846+code-rmc@users.noreply.github.com>
2021-08-03 00:12:36 -0300

    correccion de errores
    Corregido error de navegador Chrome no mostraba las respuestas v/f

--- app.js
+++ app.js
@@ -16,40 +16,48 @@ formulario.addEventListener("submit", (e) => {
 
     const dataForm = new FormData(formulario);
 
-    const respuestas = preguntas.map((value, i) => dataForm.get(`pregunta${i + 1}`));
-
-    const contador = { v: 0, f: 0 };
+    const respuestas = preguntas.map((_, i) => dataForm.get(`pregunta${i + 1}`));
 
     const correctas = correccion(respuestas);
 
+    const contador = { v: 0, f: 0 };
 
-    for (let i = 1; i <= preguntas.length; i++) {
+    preguntas.forEach((_, i) => {
 
-        correctas[i - 1] ? contador.v++ : contador.f++;
+        // Contador de V/F
+        correctas[i] ? contador.v++ : contador.f++;
 
-        document.getElementsByName(`pregunta${i}`).forEach((value) => {
-            let res = document.createElement("span");
-            let advert = document.createElement("span");
+        const listaPreguntas = document.getElementsByName(`pregunta${i + 1}`);
 
-            if (correctas[i - 1]) {
+        listaPreguntas.forEach((value) => {
+            const res = document.createElement("span");
+            const advert = document.createElement("span");
+            const fragment = document.createDocumentFragment();
+
+            if (correctas[i]) {
                 res.classList.add("badge", "bg-success");
                 res.innerHTML = '<i class="fas fa-check"></i>';
+                fragment.appendChild(res)
+                value.checked ? value.parentNode.appendChild(fragment) : null;
             } else {
                 res.classList.add("badge", "bg-danger");
                 res.innerHTML = '<i class="fas fa-times"></i>';
-
+                fragment.appendChild(res);
+                value.parentNode.appendChild(fragment);
+                // Indica la correcta
                 advert.classList.add("badge", "bg-primary");
                 advert.innerHTML = '<i class="fas fa-exclamation-circle"></i>';
-                Number(value.value) === preguntas[i - 1] ? value.appendChild(advert) : null;
+                Number(value.value) === preguntas[i] ? value.parentNode.appendChild(advert) : null;
             }
-            value.checked ? value.appendChild(res) : null;
         });
-    }
+    });
 
+    // Notificacion contador de correctas y flasas
     toastBody.innerHTML = "";
-    let totalRespuesta = document.createElement("p");
-
+    const fragment = document.createDocumentFragment();
+    const totalRespuesta = document.createElement("p");
+    totalRespuesta.classList.add("notificacion");
     totalRespuesta.innerHTML = `<span class="badge bg-success"><i class="fas fa-check"></i></span> Verdaderas: ${contador.v} </br> <span class="badge bg-danger"><i class="fas fa-times"></i></span> Flasos: ${contador.f}`;
-
-    toastBody.appendChild(totalRespuesta);
+    fragment.appendChild(totalRespuesta);
+    toastBody.appendChild(fragment);
 });
